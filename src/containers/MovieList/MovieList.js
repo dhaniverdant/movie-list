0546eb7a84ffc8f7cbd5ca20fe9e2c1e570ebc01
dhani <dhaniverdant23@gmail.com>
2021-09-17 11:44:54 +0800

    fetch movie list with axios

--- src/containers/MovieList/MovieList.js
+++ src/containers/MovieList/MovieList.js
@@ -1,6 +1,23 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setMovies } from "../../redux/actions/movieActions"
 
 const MovieList = () => {
+  const movies = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const fetchProducts = async () => {
+    const response = await axios.get('https://ghibliapi.herokuapp.com/films').catch((err) => {
+      console.log("err", err);
+    })
+    dispatch(setMovies(response.data));
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  });
+  console.log("movie list", movies);
+
   return (
     <div>MovieList</div>
   );

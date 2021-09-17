f78a53b1fb35acb6f8c8f47ae52c189b74052cad
dhani <dhaniverdant23@gmail.com>
2021-09-17 11:30:32 +0800

    create redux things

---
+++ src/redux/reducers/movieReducer.js
@@ -0,0 +1,23 @@
+import { ActionTypes } from "../constants/action-type";
+
+const initialState = {
+  movies: []
+};
+
+export const movieReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case ActionTypes.SET_MOVIES:
+      return { ...state, movies: payload };
+    default:
+      return state;
+  };
+};
+
+export const selectedMovieReducer = (state = {}, { type, payload }) => {
+  switch (type) {
+    case ActionTypes.SELECTED_MOVIE:
+      return { ...state, ...payload };
+    default:
+      return state;
+  };
+};

d2a689b3f64bc02f6b5b15b383be910d315adbaa
dhani <dhaniverdant23@gmail.com>
2021-09-17 22:59:38 +0800

    create routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import MovieDetails from "./containers/MovieDetails/MovieDetails";
 import MovieList from "./containers/MovieList/MovieList";
 
 function App() {
   return (
-    <div>
-      <MovieList />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={MovieList} />
+        <Route path="/movie/:movieId" exact component={MovieDetails} />
+      </Switch>
+    </Router>
   );
 }
 

d06dbab4912a7750383f309cbc40a80e67434dbc
dhani <dhaniverdant23@gmail.com>
2021-09-20 22:13:38 +0800

    show movie list with materialized css card

--- src/containers/MovieList/MovieList.js
+++ src/containers/MovieList/MovieList.js
@@ -1,25 +1,38 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import axios from "axios";
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { setMovies } from "../../redux/actions/movieActions"
 import MovieComponents from "../MovieComponent/MovieComponent";
 
 const MovieList = () => {
-  const movies = useSelector((state) => state);
   const dispatch = useDispatch();
   const fetchProducts = async () => {
     const response = await axios.get('https://ghibliapi.herokuapp.com/films').catch((err) => {
       console.log("err", err);
-    })
+    });
     dispatch(setMovies(response.data));
   };
 
   useEffect(() => {
     fetchProducts();
   }, []);
-  console.log("movie list", movies);
 
-  return <MovieComponents />;
+  return (
+    <div>
+      <div class="navbar-fixed">
+        <nav>
+          <div class="nav-wrapper">
+            <a href="#!" class="brand-logo">Logo</a>
+            <ul class="right hide-on-med-and-down">
+              <li><a href="/mymovie">My Movies</a></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+      <MovieComponents />
+    </div>
+  );
 };
 
 export default MovieList;
